d236b6351ee865b75b22a61835444b2fa37ddd91
saleh <salehjamaligolzar@gmail.com>
2024-07-02 11:39:46 +0200

    Re-enabled q.wait for Tensor::syncBlocking*().

--- src/core/Tensor.h
+++ src/core/Tensor.h
@@ -120,18 +120,18 @@ namespace llmsycl::core {
          * @brief Sync the device buffer to the host buffer in a blocking manner.
          */
         void syncBlockingD2H() {
-            //queue.wait();
+            queue.wait();
             queue.memcpy(hBuff, dBuff, sizeWords * sizeof(T));
-            //queue.wait();
+            queue.wait();
         }
 
         /**
          * @brief Sync the host buffer to the device buffer in a blocking manner.
          */
         void syncBlockingH2D() {
-            //queue.wait();
+            queue.wait();
             queue.memcpy(dBuff, hBuff, sizeWords * sizeof(T));
-            //queue.wait();
+            queue.wait();
         }
 
         /**
