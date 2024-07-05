72f8c8321ac66d28ce87e68086048f953f4e2fd2
saleh <salehjamaligolzar@gmail.com>
2024-07-05 11:58:43 +0200

    Improved deployment scripts. Fixed a major issue in the main camke regarding nvidia gpu arch and the cuda impl cmake script.

--- deploy/leonardo/deploy.sh
+++ deploy/leonardo/deploy.sh
@@ -7,8 +7,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../..
 rm -rf build
 mkdir build && cd build
-cmake ..
+
+# configure cmake and quit if it fails
+cmake -DIS_LEONARDO_A100=ON ..
+if [ $? -ne 0 ]; then
+    echo "cmake command failed"
+    exit 1
+fi
+
 make LLM_SYCL OrigTrain -j
+if [ $? -ne 0 ]; then
+    echo "make command failed"
+    exit 1
+fi
 
 # Request an interactive session from SLURM
 srun -n 1 -p boost_usr_prod --ntasks-per-node=1 --gres=gpu:1 -t 00:01:00 --pty /bin/bash  << EOF
