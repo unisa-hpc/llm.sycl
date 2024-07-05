d933a339ef4645bcf128b4f418f758d8473a383b
saleh <salehjamaligolzar@gmail.com>
2024-07-05 14:31:31 +0200

    Improved deploy.sh.

--- deploy/leonardo/deploy.sh
+++ deploy/leonardo/deploy.sh
@@ -1,6 +1,6 @@
 # load nvhpc for ncu
+ml cuda
 ml nvhpc
-
 source setenv.sh
 
 # get the directory path of this script
@@ -13,6 +13,11 @@ bash ../profiling/unified_profile_to_csv.sh 1 4 A100
 # Profile CUDA OrigTrain
 bash ../profiling/unified_profile_to_csv.sh 2 4 A100
 
+# Covert all A100 CSVs to PKL files.
+bash ../profiling/unified_profile_to_csv.sh 4 4 A100
+
+echo "Please copy the CSV and PKL files to your local machine (same dir as the profiling files from other GPUs) and run:"
+echo "bash ../profiling/unified_profile_to_csv.sh 5 4 FOOBAR"
 echo "Done profiling LLM_SYCL and OrigTrain."
 
 
